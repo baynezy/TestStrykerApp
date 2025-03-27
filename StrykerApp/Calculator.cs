6aaab4b0a50782361fc92606a3e11f0d8fceb074
Simon Baynes <baynezy@gmail.com>
2025-03-27 09:40:24 +0000

    Changes

---
+++ StrykerApp/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace StrykerApp;
+
+public class Calculator
+{
+    public double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public double Subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public double Multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+}
